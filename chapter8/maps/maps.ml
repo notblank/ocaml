8f62b829c9d4f02e54a42adb7c530ed876ad25fc
notblank <zert.federico@gmail.com>
2022-04-27 20:01:27 -0500

    ch8

---
+++ chapter8/maps/maps.ml
@@ -0,0 +1,22 @@
+module type Map = sig
+
+    (** [('k, 'v) t] is the type of maps that bind keys of type ['k] to values
+     * of type ['v]. *)
+    type ('k, 'v) t
+
+    (** [insert k v m] is the map [m] with [k] binding to [v]. Previous
+     * bindings of [k] are replaced by [v]. *)
+    val insert : 'k -> 'v -> ('k, 'v) t -> ('k, 'v) t
+
+    (** [find k m] is [Some v] if [k] is bound to [v] in [m] and [None] if [k]
+     * has no binding in [m].*)
+    val find : 'k -> ('k, 'v) t -> 'v option 
+
+    (** [remove k m] is the map [m] without the binding of [k]. Leaves [m]
+     * unchanged when [k] has no bindings in [m]. *)
+    val remove : 'k -> ('k, 'v) t -> ('k, 'v) t
+
+    (**  [empty] is the empty map. *)
+    val empty : ('k, 'v) t
+
+end
